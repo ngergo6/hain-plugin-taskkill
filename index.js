35e86471c3aadf9a72516f1ea0de2a895b24ff3b
Gergo Nagy L <gergo.l.nagy@ericsson.com>
2016-07-25 11:07:22 +0200

    force kill added

--- index.js
+++ index.js
@@ -18,14 +18,27 @@
                 title: trimmedQuery,
                 desc: "Kill process"
             });
+
+            res.add({
+                id: trimmedQuery,
+                payload: "force_kill",
+                title: trimmedQuery,
+                desc: "Force kill process"
+            });
         }
 
         function execute(id, payload) {
-            if (payload !== "kill") {
+            if (payload !== "kill" || payload !== "force_kill") {
                 return;
             }
 
-            var child = spawn('taskkill', ['/f', '/im', id], {
+            const args = ["/im", id];
+
+            if (payload === "force_kill") {
+                args.push("/f");
+            }
+
+            var child = spawn('taskkill', args, {
                 detached: true,
                 stdio: 'ignore',
                 cwd: os.homedir()
